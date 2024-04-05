afa81a1811f8dd80fbeb1ba03892cb6a43fa1a93
Elliott Wahl <elliottwahl@protonmail.com>
2024-04-04 18:16:39 -0700

    Created a basic GUI window

---
+++ src/UserInterface.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class UserInterface {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("CtrlLibrary");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+}
