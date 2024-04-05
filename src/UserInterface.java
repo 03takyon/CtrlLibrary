fbb5c257a7b47856f4c6113ecd42af8ccad53358
Elliott Wahl <elliottwahl@protonmail.com>
2024-04-04 21:11:30 -0700

    Adjusted frame width

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -4,7 +4,7 @@ public class UserInterface {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("CtrlLibrary");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(800, 600);
+		frame.setSize(900, 600);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
