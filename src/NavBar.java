a0d718d317acb6082e4f0ac2378263d6f5de3b53
Elliott Wahl <elliottwahl@protonmail.com>
2024-04-05 17:11:20 -0700

    Added comments

--- src/NavBar.java
+++ src/NavBar.java
@@ -3,9 +3,19 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 
 import javax.swing.JPanel;
+/**
+ * Lead Author(s):
+ * @author Elliott Wahl
+ * 
+ * Version/date: 4.5.2024.002
+ * 
+ * Responsibilities of class:
+ * 
+ */
 
+// NavBar IS-A JPanel
 public class NavBar extends JPanel {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L; // temporary, just here to get rid of warning for now
 	
 	public NavBar() {
 		this.setLayout(new FlowLayout(FlowLayout.LEFT));
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -2,16 +2,24 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.JFrame;
-
+/**
+ * Lead Author(s):
+ * @author Elliott Wahl
+ * 
+ * Version/date: 4.5.2024.003
+ * 
+ * Responsibilities of class:
+ * 
+ */
 public class UserInterface {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("CtrlLibrary");
+		JFrame frame = new JFrame("CtrlLibrary"); // UserInterface HAS-A frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(900, 600);
 		frame.setLocationRelativeTo(null);
 		frame.getContentPane().setBackground(Color.GRAY);
 		
-		NavBar navBar = new NavBar();
+		NavBar navBar = new NavBar(); // UserInterface HAS-A navBar
 		
 		frame.add(navBar, BorderLayout.NORTH);
 		
