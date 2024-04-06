b8ca9cbc4d9288af999fa26d5703772ee8062346
Elliott Wahl <elliottwahl@protonmail.com>
2024-04-05 18:18:07 -0700

    Added a pop-up window when addButton is pressed

--- src/AddGame.java
+++ src/AddGame.java
@@ -1,7 +1,10 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 /**
  * Lead Author(s):
@@ -18,6 +21,7 @@ public class AddGame extends JPanel {
 	private static final long serialVersionUID = 1L; // temporary, just here to get rid of warning for now
 	
 	private JButton addButton; // AddGame HAS-A addButton
+	private JFrame addFrame; // AddGame HAS-A addFrame
 	
 	public AddGame() {
 		this.setLayout(new BorderLayout());
@@ -25,6 +29,17 @@ public class AddGame extends JPanel {
 		addButton = new JButton("add game");
 		addButton.setBackground(Color.GRAY);
 		addButton.setFocusPainted(false);
+		
+		addFrame = new JFrame();
+		addFrame.setSize(450, 400);
+		addFrame.setLocationRelativeTo(null);
+		
+		addButton.addActionListener(new ActionListener( ) {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				addFrame.setVisible(true);
+			}
+		});
 		
 		this.add(addButton, BorderLayout.CENTER);
 	}
