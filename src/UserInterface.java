94caa40488e6015c356ca0839ace69a97eef7d00
Elliott Wahl <elliottwahl@protonmail.com>
2024-04-05 17:03:11 -0700

    Changed the background color

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 
 import javax.swing.JFrame;
 
@@ -8,6 +9,7 @@ public class UserInterface {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(900, 600);
 		frame.setLocationRelativeTo(null);
+		frame.getContentPane().setBackground(Color.GRAY);
 		
 		NavBar navBar = new NavBar();
 		
