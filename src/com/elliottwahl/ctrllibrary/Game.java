b7b0bf738408d4eedf0b7e253d2da64714f9054a
Elliott Wahl <elliottwahl@protonmail.com>
2024-04-11 17:42:09 -0700

    Added basic fields to identify Game objects

--- src/com/elliottwahl/ctrllibrary/Game.java
+++ src/com/elliottwahl/ctrllibrary/Game.java
@@ -3,11 +3,26 @@ package com.elliottwahl.ctrllibrary;
  * Lead Author(s):
  * @author Elliott Wahl
  * 
- * Version/date: 4.9.2024.001
+ * Version/date: 4.11.2024.002
  * 
  * Responsibilities of class:
  * 
  */
 public class Game {
-
+	private String title; // Game HAS-A title
+	private String genre; // Game HAS-A genre
+	private String developer; // Game HAS-A developer
+	private String executableName; // Game HAS-A executableName
+	
+	public Game(String title) {
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
 }
