18463453b8cbf59bfb7ef87eb3d0c91f67dc4ff6
Elliott Wahl <elliottwahl@protonmail.com>
2024-05-21 15:40:34 -0700

    Added more tests specific to game objects

--- src/com/elliottwahl/ctrllibrary/UnitTest.java
+++ src/com/elliottwahl/ctrllibrary/UnitTest.java
@@ -2,17 +2,46 @@ package com.elliottwahl.ctrllibrary;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import javax.swing.ImageIcon;
 
+import org.junit.jupiter.api.Test;
+/**
+ * Lead Author(s):
+ * @author Elliott Wahl
+ * 
+ * Version/date: 5.21.2024.001
+ * 
+ * Responsibilities of class: tests various aspects of the program to ensure values are assigned/retrieved as intended
+ * 
+ */
 class UnitTest {
 	private Game game;
 
+	/**
+	 * ensure various attributes of a game object are correctly assigned/retrieved
+	 */
 	@Test
-	void testGameTitle() {
-		//game = new Game("title", "path");
+	void testGame() {
+		ImageIcon icon = new ImageIcon();
+		
+		game = new Game("title", "developer", "genre", "path", icon);
 		assertEquals("title", game.getTitle());
 		game.setTitle("newTitle");
 		assertEquals("newTitle", game.getTitle());
+		
+		assertEquals("developer", game.getDeveloper());
+		game.setDeveloper("newDeveloper");
+		assertEquals("newDeveloper", game.getDeveloper());
+		
+		assertEquals("genre", game.getGenre());
+		game.setGenre("newGenre");
+		assertEquals("newGenre", game.getGenre());
+		
+		assertEquals("path", game.getPath());
+		
+		ImageIcon newIcon = new ImageIcon();
+		assertEquals(icon, game.getIcon());
+		game.setIcon(newIcon);
+		assertEquals(newIcon, game.getIcon());
 	}
-
 }
